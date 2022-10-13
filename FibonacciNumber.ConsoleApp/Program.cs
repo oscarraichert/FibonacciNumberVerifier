81e487913410f58f83f2d1483ba2ae09033b7603
oscarraichert <oscarraichert@hotmail.com>
2022-10-13 06:37:18 -0300

    Add project files.

---
+++ FibonacciNumber.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(VerifyNumber(55));
+
+string VerifyNumber(int inputNumber)
+{
+    var currentNumber = 0;
+
+    List<int> fibonacci = new()
+    {
+        0, 1
+    };
+
+    var array = fibonacci.ToArray();
+
+    while (inputNumber > currentNumber)
+    {
+        var lastTwoNumbers = fibonacci.GetRange(fibonacci.Count - 2, 2);
+
+        currentNumber = lastTwoNumbers[0] + lastTwoNumbers[1];
+
+        fibonacci.Add(currentNumber);
+
+        Console.WriteLine(currentNumber);
+    }
+
+    if (currentNumber == inputNumber)
+    {
+        return "\nO número está na sequência de fibonacci";
+    }
+
+    return "\nO número não está na sequência de fibonacci";
+}
